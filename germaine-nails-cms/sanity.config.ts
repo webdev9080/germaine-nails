a8b155dd90cbb2e0995fcf42f5edb4c8af91322f
webdev9080 <webdev9080@gmail.com>
2025-06-25 15:57:40 +0000

    Clean studio integration after separation

--- germaine-nails-cms/sanity.config.ts
+++ germaine-nails-cms/sanity.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -10,7 +9,7 @@ export default defineConfig({
   projectId: '0k4zazrm',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool()],
 
   schema: {
     types: schemaTypes,
