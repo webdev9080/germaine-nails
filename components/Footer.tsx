528898f15bdb80b993c994057d9adc310a51d728
webdev9080 <webdev9080@gmail.com>
2025-06-28 20:17:58 +0000

    Ajout de la page politique-confidentialite et amélioration de footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 // components/Footer.tsx
+import Link from "next/link";
 import { FaFacebookF, FaWhatsapp, FaTiktok } from "react-icons/fa";
 
 export default function Footer() {
@@ -11,7 +12,7 @@ export default function Footer() {
             href="https://wa.me/22870306224"
             target="_blank"
             rel="noopener noreferrer"
-            className=" fs-4"
+            className="fs-4"
           >
             <FaWhatsapp className="hover-icon" />
           </a>
@@ -19,7 +20,7 @@ export default function Footer() {
             href="https://www.facebook.com/germaine.nails"
             target="_blank"
             rel="noopener noreferrer"
-            className=" fs-4"
+            className="fs-4"
           >
             <FaFacebookF className="hover-icon" />
           </a>
@@ -27,12 +28,24 @@ export default function Footer() {
             href="https://www.tiktok.com/@germaine.nails"
             target="_blank"
             rel="noopener noreferrer"
-            className=" fs-4"
+            className="fs-4"
           >
             <FaTiktok className="hover-icon" />
           </a>
         </div>
-        <p className="mb-0">&copy; 2025 Germaine Nails. Tous droits réservés.</p>
+
+        <div className="mt-3">
+          <Link
+            href="/politique-confidentialite"
+            className="text-decoration-underline text-muted small"
+          >
+            Politique de confidentialité
+          </Link>
+        </div>
+
+        <p className="mb-0 mt-2 text-muted small">
+          &copy; 2025 Germaine Nails. Tous droits réservés.
+        </p>
       </div>
     </footer>
   );
