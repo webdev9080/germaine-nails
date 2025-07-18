a2b91dcd6f7985683e7455f51c7052212f189a04
webdev9080 <webdev9080@gmail.com>
2025-07-18 12:01:58 +0000

    ✨ Modifications du metadata

--- utils/metadata.ts
+++ utils/metadata.ts
@@ -14,7 +14,7 @@ export function generateMetadata({
   path = "/",
   noIndex = false,
 }: MetaOptions): Metadata {
-  const baseUrl = "https://germaine-nails-tg.vercel.app";
+  const baseUrl = "https://germaine-nails.dedyn.io";
   const defaultImage = `${baseUrl}/images/og-default.jpg`;
 
   return {
